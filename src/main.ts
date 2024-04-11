d1827676930f9b0ed08f4b9e42f7065492456d09
Ivan Gonzalez Perez <ivangp61@gmail.com>
2024-04-10 21:36:10 -0400

    Patch update 3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import * as core from '@actions/core'
+import * as event from './event'
+import * as version from './version'
+import * as git from './git'
+import * as github from './github'
 
 export async function run(): Promise<void> {
   try {
-    core.setOutput('release-url', 'https://example.com')
+    const token = core.getInput('repo-token');
+    const tag = event.getCreatedTag();
+    var releaseUrl = '';
+
+    if(tag && version.isSemVer(tag)){
+      const changeLog = await git.getChangesIntroducedByTag(tag);
+
+      releaseUrl = await github.createReleaseDraft(tag, token, changeLog);
+    }
+
+    
+    core.setOutput('release-url', releaseUrl);
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
