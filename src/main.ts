2aa096af5e0a573dc967ae34aafd24491f724a5d
Ivan Gonzalez Perez <ivangp61@gmail.com>
2024-04-12 09:25:39 -0400

    the url

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core'
 export async function run(): Promise<void> {
   try {
 
-    core.setOutput('tag', 'my tag')
+    core.setOutput('release-url', 'the url')
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
