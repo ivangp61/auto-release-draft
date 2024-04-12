6645521ec3e69515e83b54ba35fe4969168edc01
Ivan Gonzalez Perez <ivangp61@gmail.com>
2024-04-12 14:11:34 -0400

    Multiple Updates 3

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
-import * as core from '@actions/core'
+import * as core from '@actions/core';
+import * as event from './event';
+import * as version from './version';
+// import * as github from '@actions/github';
 
 export async function run(): Promise<void> {
   try {
-    const myTag = 'my tag'
+    const tag = event.getCreatedTag();
+    // let releaseUrl = '';
 
     // console.log(myTag);
 
-    core.setOutput('release-url', myTag)
-    core.setOutput('tag', myTag)
+    if (tag && version.isSemVer(tag)) {
+      // const changeLog = await git.getChangesIntroducedByTag(tag)
+
+      // releaseUrl = await github.createReleaseDraft(tag, token, changeLog)
+    }
+
+    core.setOutput('release-url', tag)
+    core.setOutput('tag', tag)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
