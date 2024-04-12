847b569f6e6466cc19139edda3ce14a63b181aae
Ivan Gonzalez Perez <ivangp61@gmail.com>
2024-04-12 09:21:12 -0400

    my tag 2

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
 import * as core from '@actions/core'
-import * as event from './event'
-import * as version from './version'
-import * as git from './git'
-import * as github from './github'
 
 export async function run(): Promise<void> {
   try {
-    const token = core.getInput('repo-token')
-    const tag = event.getCreatedTag()
-    let releaseUrl = ''
 
-    if (tag && version.isSemVer(tag)) {
-      const changeLog = await git.getChangesIntroducedByTag(tag)
-
-      releaseUrl = await github.createReleaseDraft(tag, token, changeLog)
-    }
-
-    // core.setOutput('release-url', releaseUrl)
     core.setOutput('tag', 'my tag')
   } catch (error) {
     // Fail the workflow run if an error occurs
