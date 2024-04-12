d1620fd94a8fd8eeddbbf047cac0d1c2031cde77
Ivan Gonzalez Perez <ivangp61@gmail.com>
2024-04-12 09:41:25 -0400

    updated the main test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -10,5 +10,6 @@ describe('When running the action', () => {
   test('it should set the release-url output parameter', async () => {
     await run()
     expect(fakeSetOutput).toHaveBeenCalledWith('release-url', expect.anything())
+    expect(fakeSetOutput).toHaveBeenCalledWith('tag', expect.anything())
   })
 })
