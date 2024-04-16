93b0d4e1026aac69aaa962101cfce9f8cccd1890
Ivan Gonzalez Perez <ivangp61@gmail.com>
2024-04-15 20:17:59 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 import * as core from '@actions/core'
-import * as event from './event'
-import * as version from './version'
-import * as git from './git'
-import * as github from './github';
+// import * as event from './event'
+// import * as version from './version'
+// import * as git from './git'
+// import * as github from './github';
 
 
 export async function run(): Promise<void> {
   try {
-    const token = core.getInput('repo-token');
+    // const token = core.getInput('repo-token');
 
-    const tag = event.getCreatedTag();
-    let releaseUrl = '';
+    // const tag = event.getCreatedTag();
+    // let releaseUrl = '';
 
-    if (tag && version.isSemVer(tag)) {
-      const changeLog = await git.getChangesIntroducedByTag(tag);      
+    // if (tag && version.isSemVer(tag)) {
+    //   const changeLog = await git.getChangesIntroducedByTag(tag);      
 
-      releaseUrl = await github.createReleaseDraft(tag, token, changeLog);
-    }
+    //   releaseUrl = await github.createReleaseDraft(tag, token, changeLog);
+    // }
 
     core.setOutput('event-name', 'some event name');
-    core.setOutput('release-url', releaseUrl);
+    core.setOutput('release-url', 'some url');
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) {
